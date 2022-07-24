b2d4486b0d935487ee4ad38a69ac5ff105e236d1
Retr() <96971387+Retr0-dot-cpp@users.noreply.github.com>
2022-07-24 11:05:21 +0400

    Adding comments and fixing bugs

--- main.cpp
+++ main.cpp
@@ -29,27 +29,31 @@ int main()
     setcolor(YELLOW);
     line(0,99, 100,99);
 	for(i=1; i<maxY; i++){
+	//if the pixel underneath is black
 	if(getpixel(x, y+i)==0){
 		putpixel(x,y+i, YELLOW);
 		putpixel(x,y-1+i, BLACK);
 		delay(10);
 	}
-		else if(getpixel(x, y+i)==14 && getpixel(x-1, y+i) == 0){
+	//if the pixel underneath is yellow and black on the right
+		else if(getpixel(x, y+i)==14 && getpixel(x+1, y+i) == 0){
       		putpixel(x+1, y+i, YELLOW);
       		putpixel(x, y+i-1, BLACK);
 			delay(10);
 		}
-		else if(getpixel(x,y+i)==14 && getpixel(x-1,y+i))
-		else if(getpixel(x+1, y+i)==14 && getpixel(x, y+i)==14 && getpixel(x-1, y+i)==0 ){
+	//if the pixel underneath is yellow, yellow on the left and black on the right
+		else if(getpixel(x,y+i)==14 && getpixel(x-1,y+i)==14 && getpixel(x+1, y+i)==0){
+			putpixel(x+1,y+i, YELLOW);
+			putpixel(x, y+i-1, BLACK);
+			delay(10);
+		}
+	//if the pixel underneath is yellow, yellow on the right and black on the left
+		else if(getpixel(x, y+i)==14 && getpixel(x+1, y+i)==14 && getpixel(x-1, y+i)==0 ){
 			
 			putpixel(x-1, y+i, YELLOW);  
 			putpixel(x, y+i-1, BLACK);
 			delay(10);
 		}
-	/*	else if(getpixel(x)){
-			putpixel()
-		}
-		else if{}*/
 		else {
 			i--;
 		break;
@@ -63,5 +67,4 @@ int main()
   closegraph();
   return 0;
 }
-
 
